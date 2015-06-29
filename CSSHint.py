1a161c475cdef9f40b8fe531e514fdb94f9995d9
xiaowu <fe.xiaowu@gmail.com>
2015-06-29 14:03:40 +0800

    add files

---
+++ CSSHint.py
@@ -0,0 +1,27 @@
+import os
+
+import sublime
+import sublime_plugin
+
+
+class CsshintCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        filepath = self.view.file_name()
+        packages = sublime.packages_path()
+        args = {
+            "cmd": [
+                "csshint",
+                filepath,
+                # "--reporter",
+                # os.path.join(packages, "CSSHint", "reporter.js")
+            ],
+            "file_regex": r"CSSHint: (.+)\]",
+            "line_regex": r"(\d+),(\d+): (.*)$"
+        }
+
+        if sublime.platform() == "windows":
+            args['cmd'][0] += ".cmd"
+        elif sublime.platform() == "osx":
+            args['path'] = "/usr/local/share/npm/bin:/usr/local/bin:/opt/local/bin"
+
+        self.view.window().run_command('exec', args)
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # sublime-csshint
 csshint for sublime
+
+## required
+
+* [https://github.com/ielgnaw/node-csshint](https://github.com/ielgnaw/node-csshint)
+
+## install
+
+1. `[sudo] npm install csshint -g`
+2. `git clone https://github.com/xuexb/sublime-csshint.git`
+3. `mv ./sublime-csshint ${packages}`
+4. add `.csshintrc` for your Project root
+
+## .csshintrc
+
+[demo](.csshintrc)
+
+## links
+
+* [https://github.com/ecomfe/spec/blob/master/css-style-guide.md](https://github.com/ecomfe/spec/blob/master/css-style-guide.md)
+* [https://github.com/ielgnaw/node-csshint/blob/master/lib/config.js](https://github.com/ielgnaw/node-csshint/blob/master/lib/config.js)
